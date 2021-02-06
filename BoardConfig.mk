f9f18912cd0527d5535145459622e1800eac5f25
Rahul Talukdar <70837932+Ryukendo9@users.noreply.github.com>
2021-02-06 14:30:49 +0530

    jasmine_sprout: Adapt for shrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,5 +49,42 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # Platform
 BOARD_VENDOR_PLATFORM := xiaomi-sdm660
 
+TARGET_USERIMAGES_USE_F2FS := true
+
 # WLAN MAC
 WLAN_MAC_SYMLINK := true
+
+@@ -83,6 +84,7 @@ ALLOW_MISSING_DEPENDENCIES := true
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FBE := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_METADATA_PARTITION := true
+@@ -92,3 +94,23 @@ BOARD_USES_METADATA_PARTITION := true
+#TARGET_NO_RECOVERY := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+AB_OTA_UPDATER := tr
+
+
+# SHRP Configuration
+SHRP_PATH := device/xiaomi/jasmine_sprout
+SHRP_MAINTAINER := Ryukendo9
+SHRP_DEVICE_CODE := jasmine_sprout
+SHRP_EDL_MODE := 1
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb-otg
+SHRP_FLASH := 1
+SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
+SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
+SHRP_FONP_3 := /sys/class/leds/led:torch_2/brightness
+SHRP_FONP_4 := /sys/class/leds/led:switch_0/brightness
+SHRP_FONP_5 := /sys/class/leds/led:switch_1/brightness
+SHRP_FLASH_MAX_BRIGHTNESS := 100
+SHRP_AB := true
+SHRP_REC_TYPE := Treble
+SHRP_DEVICE_TYPE := A/B
+SHRP_OFFICIAL := true
+SHRP_EXPRESS := true
+
